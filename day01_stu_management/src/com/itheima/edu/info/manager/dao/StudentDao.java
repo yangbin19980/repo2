b85b3843c0404c94756935daaf75aa242b7500c3
uncleyang <1748121899@qq.com>
2022-03-16 17:28:23 +0800

    1

--- day01_stu_management/src/com/itheima/edu/info/manager/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.edu.info.manager.dao;
-
-import com.itheima.edu.info.manager.domain.Student;
-
-public class StudentDao {
-    public boolean addStudent(Student stu){
-        return false;
-    }
-}
